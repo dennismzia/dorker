3d0e595967e526f06107846ed21208b50e32f84c
Dennis malware <davspencer30@gmail.com>
2022-05-24 20:44:50 +0300

    changed the driver from firefox to chrome

--- dorker.py
+++ dorker.py
@@ -3,7 +3,11 @@ import argparse
 import sys
 from helium import *
 import os
-
+from bs4 import BeautifulSoup
+import requests
+import lxml
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
 
 parser = argparse.ArgumentParser(description="handy google dorker by SirMalware",epilog='\tExample: \r\npython ' + sys.argv[0] + " -d 'THE-DORK-YOU-WANT'")
 parser._optionals.title = "OPTIONS"
@@ -20,10 +24,15 @@ q = args.dork
 num = args.number
 url = 'https://www.google.com/search?q='+q+'&num='+num+'&start='+str(pages) if args.pages else 'https://www.google.com/search?q='+q+'&num='+num
 print(url)
-driver = helium.start_firefox(url,headless=True)
+# url = "https://www.google.com/search?q=inurl: security&num=50&start=0"
+
+helium.set_driver("/usr/local/bin/chromedriver")
+driver = helium.start_chrome(url,headless=True)
 urls = helium.find_all(S('.yuRUbf'))
+print(helium.get_driver())
 url = [i.web_element.find_element_by_tag_name('a').get_attribute('href') for i in urls]
 li = set(url)
+# print(li)
 if (url == []): kill_browser();print("urls empty means nothing found");os.system("exit")
 
 if(args.output):
