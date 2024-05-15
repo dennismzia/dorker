4da156c2a92cd3e8a24209f0a1ea88799aa959bf
Dennis malware <davspencer30@gmail.com>
2024-05-15 16:41:25 +0300

    updated the dorker to work with modern chrome

--- dorker.py
+++ dorker.py
@@ -2,7 +2,7 @@
 import argparse
 import sys
 from helium import *
-import os
+import os,re
 from bs4 import BeautifulSoup
 import requests
 import lxml
@@ -27,13 +27,25 @@ print(url)
 # url = "https://www.google.com/search?q=inurl: security&num=50&start=0"
 
 helium.set_driver("/usr/local/bin/chromedriver")
+# below we start chrome in headless mode.
 driver = helium.start_chrome(url,headless=True)
 urls = helium.find_all(S('.yuRUbf'))
-print(helium.get_driver())
-url = [i.web_element.find_element_by_tag_name('a').get_attribute('href') for i in urls]
-li = set(url)
+# print(helium.get_driver())
+print(url)
+print()
+# print(urls[0])
+pattern = r"\b(?:http|https)://\S*"
+# print(re.findall(pattern,(url.web_element.text))[0])
+# print(urls[0].web_element.find_element_by_tag_name('a').get_attribute('href'))
+# print(anchor[0].web_element.get_attribute("href"))
+# url = [i.web_element.find_element_by_tag_name('a').get_attribute('href') for i in urls]
+li = set()
+for url in urls:
+    url2 = re.findall(pattern,url.web_element.text)[0]
+    li.add(url2)
+
 # print(li)
-if (url == []): kill_browser();print("urls empty means nothing found");os.system("exit")
+if (li == []): kill_browser();print("urls empty means nothing found");os.system("exit")
 
 if(args.output):
     file1 = open(args.output+'.txt', 'w')
@@ -46,5 +58,6 @@ else:
         for i in list(li):
             print(i)
             file.write(i.strip()+'\n')
+print()
 print("results automatically saved to output.txt file")
 kill_browser()
