63e5a0ab4fed9e368af9b2ea6b2cd4119451af9a
Dennis malware <davspencer30@gmail.com>
2024-06-04 16:00:20 +0300

    changed code from helium back to selenium web driver

--- dorker.py
+++ dorker.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import argparse
 import sys
-from helium import *
 import os,re
-from bs4 import BeautifulSoup
-import requests
-import lxml
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 
 parser = argparse.ArgumentParser(description="handy google dorker by SirMalware",epilog='\tExample: \r\npython ' + sys.argv[0] + " -d 'THE-DORK-YOU-WANT'")
 parser._optionals.title = "OPTIONS"
@@ -26,27 +25,18 @@ url = 'https://www.google.com/search?q='+q+'&num='+num+'&start='+str(pages) if a
 print(url)
 # url = "https://www.google.com/search?q=inurl: security&num=50&start=0"
 
-helium.set_driver("/usr/local/bin/chromedriver")
-# below we start chrome in headless mode.
-driver = helium.start_chrome(url,headless=True)
-urls = helium.find_all(S('.yuRUbf'))
-# print(helium.get_driver())
-print(url)
 print()
-# print(urls[0])
-pattern = r"\b(?:http|https)://\S*"
-# print(re.findall(pattern,(url.web_element.text))[0])
-# print(urls[0].web_element.find_element_by_tag_name('a').get_attribute('href'))
-# print(anchor[0].web_element.get_attribute("href"))
-# url = [i.web_element.find_element_by_tag_name('a').get_attribute('href') for i in urls]
-li = set()
-for url in urls:
-    url2 = re.findall(pattern,url.web_element.text)[0]
-    li.add(url2)
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+ 
+driver = webdriver.Chrome(options=options)
+driver.get(url)
+elements = driver.find_elements(By.CLASS_NAME, "yuRUbf")
+urls = [elem.find_element(By.TAG_NAME, "a").get_attribute("href") for elem in elements]
 
+li = set(urls)
+if (url == []): kill_browser();print("urls empty means nothing found");os.system("exit")
 # print(li)
-if (li == []): kill_browser();print("urls empty means nothing found");os.system("exit")
-
 if(args.output):
     file1 = open(args.output+'.txt', 'w')
     for i in list(li):
@@ -60,4 +50,4 @@ else:
             file.write(i.strip()+'\n')
 print()
 print("results automatically saved to output.txt file")
-kill_browser()
+driver.quit()
